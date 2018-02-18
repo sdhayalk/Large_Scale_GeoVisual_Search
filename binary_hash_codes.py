2019e9f2d571eebb94ab11743ce132ad52c83c1e
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-02-18 03:00:43 -0700

    implementing caffe test

--- binary_hash_codes.py
+++ binary_hash_codes.py
@@ -2,11 +2,16 @@ import caffe
 
 
 def main():
-	caffe.set_device(0)
-	caffe.set_mode_gpu()
+	USE_GPU = True
+	if USE_GPU:
+	    caffe.set_device(0)
+	    caffe.set_mode_gpu()
+	else:
+	    caffe.set_mode_cpu()
 
-	
-
+	CNN_NETWORK_PATH = "ResNet-101-deploy.prototxt"
+	CAFFEMODEL_PATH =  "G:/DL/large_scale_geovisual_search/models/ResNet-101-model.caffemodel"
+	net = caffe.Net(CNN_NETWORK_PATH, CAFFEMODEL_PATH, caffe.TEST)
 
 
 if __name__ == '__main__':
